a343c64504a05beb4153ac8950afb6d0e9497037
syamgot <syamgot@gmail.com>
2016-07-19 14:54:09 +0900

    タブを修正

--- mask/mask.cpp
+++ mask/mask.cpp
@@ -3,20 +3,20 @@
 int main(int argc, const char *argv[]) {
 
 	// 変数定義
-    cv::Mat im, mask, dst;
+	cv::Mat im, mask, dst;
 
 	// 画像の準備
-    im 	 = cv::imread("./data/image.jpg");
-    mask = cv::imread("./data/mask.jpg");
+	im 	 = cv::imread("./data/image.jpg");
+	mask = cv::imread("./data/mask.jpg");
 
 	// マスク
 	dst = im.clone();
 	cv::bitwise_and(im, mask, dst);
 
 	// 出力&キー受付
-    cv::imshow("out", dst);
-    cv::waitKey();
+	cv::imshow("out", dst);
+	cv::waitKey();
 
-    return 0;
+	return 0;
 
 }
