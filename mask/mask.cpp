4e975631ffaea232cd342599535b1e0dc0231be2
syamgot <syamgot@gmail.com>
2016-07-19 14:52:32 +0900

    マスク処理を追加

---
+++ mask/mask.cpp
@@ -0,0 +1,22 @@
+#include "opencv2/opencv.hpp"
+
+int main(int argc, const char *argv[]) {
+
+	// 変数定義
+    cv::Mat im, mask, dst;
+
+	// 画像の準備
+    im 	 = cv::imread("./data/image.jpg");
+    mask = cv::imread("./data/mask.jpg");
+
+	// マスク
+	dst = im.clone();
+	cv::bitwise_and(im, mask, dst);
+
+	// 出力&キー受付
+    cv::imshow("out", dst);
+    cv::waitKey();
+
+    return 0;
+
+}
