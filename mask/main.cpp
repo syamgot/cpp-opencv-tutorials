d38e267ec8119b7af4d431ea72633b20105949ef
syamgot <syamgot@gmail.com>
2016-07-19 15:33:01 +0900

    変数名変更 画像読み込み判定を追加

--- mask/main.cpp
+++ mask/main.cpp
@@ -3,15 +3,18 @@
 int main(int argc, const char *argv[]) {
 
 	// 変数定義
-	cv::Mat im, mask, dst;
+	cv::Mat src, mask, dst;
 
 	// 画像の準備
-	im 	 = cv::imread("./data/image.jpg");
+	src  = cv::imread("./data/image.jpg");
 	mask = cv::imread("./data/mask.jpg");
+	if(src.empty() || mask.empty()) {
+		return -1;
+	}
 
 	// マスク
-	dst = im.clone();
-	cv::bitwise_and(im, mask, dst);
+	dst = src.clone();
+	cv::bitwise_and(src, mask, dst);
 
 	// 出力&キー受付
 	cv::imshow("out", dst);
