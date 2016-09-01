73ae4c804fd6b19f0ac0405c61bc57f981e98621
syamgot <syamgot@gmail.com>
2016-09-01 12:45:06 +0900

    data,ポインタを使ったアクセスを追加. 1チャンネルの処理を追加

--- access_to_pixel/main.cpp
+++ access_to_pixel/main.cpp
@@ -1,27 +1,92 @@
 #include "opencv2/opencv.hpp"
 
+using namespace std;
+using namespace cv;
+
 int main(int argc, char *argv[])
 {
-	cv::Mat src;
-	src = cv::imread("./data/image.jpg");
+	Mat src, dst;
+
+
+	// ----------------------------------------
+	// 3チャンネル
+	// 
+
+	src = imread("./data/image.jpg");
 
 	// cv::Mat::at 
-	for (int y = 0; y < src.rows; y++) {
-		for (int x = 0; x < src.cols; x++) {
-			(src.at<cv::Vec<unsigned char,3 > >(y,x))[0] = 255;
-			(src.at<cv::Vec<unsigned char,3 > >(y,x))[1] = 255;
-			(src.at<cv::Vec<unsigned char,3 > >(y,x))[2] = 255;
+	dst = src.clone();
+	for (int y = 0; y < dst.rows; y++) {
+		for (int x = 0; x < dst.cols; x++) {
+			// ネガポジ反転
+			dst.at<Vec3b>(y,x)[0] = ~dst.at<Vec3b>(y,x)[0]; // B
+			dst.at<Vec3b>(y,x)[1] = ~dst.at<Vec3b>(y,x)[1]; // G
+			dst.at<Vec3b>(y,x)[2] = ~dst.at<Vec3b>(y,x)[2]; // R
 		}
 	}
+	imshow("out", dst);
+	waitKey();
 
 	// cv::MatIterator_
-	cv::MatIterator_<cv::Vec<unsigned char, 3> > it 	= src.begin<cv::Vec<unsigned char, 3> >();
-	cv::MatIterator_<cv::Vec<unsigned char, 3> > it_end = src.end<cv::Vec<unsigned char, 3> >();
+	dst = src.clone();
+	MatIterator_<Vec3b> it 		= dst.begin<Vec3b>();
+	MatIterator_<Vec3b> it_end 	= dst.end<Vec3b>();
 	for(; it != it_end; it++) {
-		(*it)[0] = 255;
-		(*it)[1] = 255;
-		(*it)[2] = 255;
+		// シャッフル
+		int b = (*it)[0]; int g = (*it)[1]; int r = (*it)[2];
+		(*it)[0] = r;
+		(*it)[1] = b;
+		(*it)[2] = g;
 	}
+	imshow("out", dst);
+	waitKey();
+
+	// data
+	dst = src.clone();
+	for(int y = 0; y < dst.rows; y++){
+		for(int x = 0; x < dst.cols; x++){
+			for(int c = 0; c < dst.channels(); c++){
+				int i = y * dst.step + x * dst.elemSize() + c;
+				dst.data[i] = ~dst.data[i];
+			}
+		}
+	}
+	imshow("out", dst);
+	waitKey();
+
+	// ポインタ
+	dst = src.clone();
+	for(int y = 0; y < dst.rows; y++){
+		for(int x = 0; x < dst.cols; x++){
+			Vec3b *ptr = dst.ptr<Vec3b>(y);
+			// シャッフル
+			int b = (*ptr)[x*3+0]; 
+			int g = (*ptr)[x*3+1]; 
+			int r = (*ptr)[x*3+2];
+			(*ptr)[x*3+0] = g;
+			(*ptr)[x*3+1] = b;
+			(*ptr)[x*3+2] = r;
+		}
+	}
+	imshow("out", dst);
+	waitKey();
+
+
+
+	// ----------------------------------------
+	// 1チャンネル
+	// 
+
+	src = imread("./data/image.jpg", 0);
+	dst = src.clone();
+	for(int y = 0; y < dst.rows; y++){
+		for(int x = 0; x < dst.cols; x++){
+			dst.at<uchar>(y,x) = ~dst.at<uchar>(y,x);
+		}
+	}
+	imshow("out", dst);
+	waitKey();
+
 
 	return 0;
 
